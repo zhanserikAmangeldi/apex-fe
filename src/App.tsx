4280dd4056634ac0e5c72693965da218b1e3f68f
zhanserikAmangeldi <amangeldi.janserik2017@gmail.com>
2025-12-07 21:44:04 +0500

    feat[general]: add welcome page and auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import './index.css'
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthProvider.tsx";
 
-function App() {
+import { WelcomePage } from "./pages/WelcomePage.tsx";
+import { LoginPage } from "./pages/LoginPage.tsx";
+import { RegisterPage } from "./pages/RegisterPage.tsx";
+import { ForgotPasswordPage } from "./pages/ForgotPasswordPage.tsx";
+
+export default function App() {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-red-900">Hello World!</h1>
-        </div>
+        <AuthProvider>
+            <BrowserRouter>
+                <div style={{ fontFamily: "'Noto Sans', sans-serif" }}>
+
+                    <Routes>
+                        <Route path="/" element={<WelcomePage />} />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+
+                        <Route path="*" element={<Navigate to="/" replace />} />
+                    </Routes>
+
+                </div>
+            </BrowserRouter>
+        </AuthProvider>
     );
 }
-
-export default App
